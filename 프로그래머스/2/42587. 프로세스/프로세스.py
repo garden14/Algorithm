af4e95638825a16c025817a913c808c905223b18
garden14 <113659080+garden14@users.noreply.github.com>
2024-08-18 14:19:55 +0900

    [level 2] Title: 프로세스, Time: 0.89 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    queue = deque((idx,i) for idx, i in enumerate(priorities))
+    
+    while True:
+        current = queue.popleft()
+        if any(current[1] < q[1] for q in queue):
+            queue.append(current)
+        else:
+            answer += 1
+            if current[0] == location:
+                return answer
