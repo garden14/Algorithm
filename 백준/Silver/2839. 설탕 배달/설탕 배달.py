cf536e3130800e80176d3fd43a656a4cc4ef0d4e
garden14 <113659080+garden14@users.noreply.github.com>
2024-09-05 14:08:06 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+if N % 5 == 0 :
+    print(N // 5)
+
+else :
+    count = 0
+    while N > 0 :
+        N -= 3
+        count += 1
+        
+        if N % 5 == 0 :
+            count += N // 5
+            print(count)
+            break
+        
+        elif N == 1 or N == 2 :
+            print(-1)
+            break
+        
+        elif N == 0 :
+            print(count)
+            break
