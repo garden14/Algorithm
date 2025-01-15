aa7c63132f53a7ccaa6d9161bfa698d6d0c321a3
garden14 <113659080+garden14@users.noreply.github.com>
2025-01-15 13:20:37 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 16420 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,32 +1,33 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
     static int n;
-    static char[][] painting;
+    static char[][] arr;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
 
-        painting = new char[n][n];
+        n = Integer.parseInt(br.readLine());
+        arr = new char[n][n];
         visited = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < n; j++) {
-                painting[i][j] = line.charAt(j);
+                arr[i][j] = line.charAt(j);
             }
         }
 
         int normal = 0;
-        int colorblind = 0;
+        int colorBlind = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -36,30 +37,31 @@ public class Main {
                 }
             }
         }
+
         visited = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (!visited[i][j]) {
-                    bfs(i, j, true); // 색맹인 경우
-                    colorblind++;
+                    bfs(i, j, true);
+                    colorBlind++;
                 }
             }
         }
 
-        System.out.println(normal + " " + colorblind);
+        System.out.println(normal+ " " + colorBlind);
 
     }
 
-    public static void bfs(int x, int y, boolean isBlind) {
+    public static void bfs(int x, int y, boolean colorBlind) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{x, y});
         visited[x][y] = true;
 
-        char startColor = painting[x][y];
+        char currentColor = arr[x][y];
 
-        if (isBlind && (startColor == 'R' || startColor == 'G')) {
-            startColor = 'X';
+        if (colorBlind && (currentColor == 'R' || currentColor == 'G')) {
+            currentColor = 'X';
         }
 
         while (!queue.isEmpty()) {
@@ -70,17 +72,16 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny]) {
-                    char nextColor = painting[nx][ny];
-                    if (isBlind && (nextColor == 'R' || nextColor == 'G')) {
+                    char nextColor = arr[nx][ny];
+                    if (colorBlind && (nextColor == 'R' || nextColor == 'G')) {
                         nextColor = 'X';
                     }
-
-                    if (nextColor == startColor) {
+                    if (currentColor == nextColor) {
                         visited[nx][ny] = true;
                         queue.offer(new int[]{nx, ny});
                     }
+
                 }
             }
         }
