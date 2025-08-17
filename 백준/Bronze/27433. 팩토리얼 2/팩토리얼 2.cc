c1125f598518234b98663e5d1b79026015483db0
garden14 <113659080+garden14@users.noreply.github.com>
2025-08-17 20:46:48 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    
+    long long result = 1;
+
+    for(int i = 2; i <= N; i++) {
+        result *= i;
+    }
+    
+    cout << result;
+
+    return 0;
+}
