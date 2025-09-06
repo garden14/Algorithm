7202da4ed74908b557c0030b0b43b270755fd4fa
garden14 <113659080+garden14@users.noreply.github.com>
2025-09-06 21:32:11 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int ans = 1e9;
+    vector<pair<int,int>> v;
+
+    int n; 
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        int a, b; 
+        cin >> a >> b;
+        v.push_back({a, b});
+    }
+
+
+    for(int i = 1; i < (1 << n); ++i){ 
+        int a = 1, b = 0;
+        for(int x = 0; x < n; ++x){ 
+            if(i & (1 << x)){  
+                a *= v[x].first;
+                b += v[x].second;
+            }
+        }
+        ans = min(ans, abs(a - b));
+    }
+    
+    cout << ans;
+    return 0;
+}
